97f81e9994b230b664ded6e19350cc53a1b07e5f
Federico Jose Cuda <fjcuda@gmail.com>
2025-01-08 22:13:23 -0300

    cambie una cosa en el views

--- Mi_Restaurante/AppRestaurante/views.py
+++ Mi_Restaurante/AppRestaurante/views.py
@@ -8,7 +8,7 @@ def cargar_cocinero(request):
         form = CocineroForms(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('inicio')
+            return redirect('inicio.html')
     else: 
         form = CocineroForms()
     return render(request, 'AppRestaurante/cargar_cocinero.html', {'form': form})
