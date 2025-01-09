6714d7dcaef525fa1e408238bdd1738e0eb2bab0
Federico Jose Cuda <fjcuda@gmail.com>
2025-01-08 21:52:27 -0300

    Agregue inicio.html, cargcar_cocinero funciona y cambie algunas urls

--- Mi_Restaurante/AppRestaurante/urls.py
+++ Mi_Restaurante/AppRestaurante/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import cargar_cocinero, mostrar_reservas
 
 urlpatterns = [
-    path('cargar_cocinero/', cargar_cocinero, name='cargar_cocinero'),
+    path('cargar_cocinero', cargar_cocinero, name='cargar_cocinero'),
     path('reservas/', mostrar_reservas, name='mostrar_reservas'),
 ]
--- Mi_Restaurante/AppRestaurante/views.py
+++ Mi_Restaurante/AppRestaurante/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import CocineroForms, Comentario_acerca_restauranteForms, ReservaForms
 from .models import Cocinero, Comentario_acerca_restaurante, Reserva
+# Create your views here.
 
 def cargar_cocinero(request):
     if request.method == 'POST':
         form = CocineroForms(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('FALATA PONER ALGHO')
+            return redirect('inicio')
     else: 
         form = CocineroForms()
-    return render(request, 'AppRestaurante/cargar_cocinero_form.html', {'form': form})
+    return render(request, 'AppRestaurante/cargar_cocinero.html', {'form': form})
 
 def mostrar_reservas(request):
     reservas = Reserva.objects.all()
